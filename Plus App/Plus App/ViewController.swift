cfdc613549bbb7da7dba17ffd82c4e92cb8605ee
Fatan <Faten.Alfifi.jiz.s@tuwaiq.edu.sa>
2021-12-23 17:01:59 +0300

    create init user interface

---
+++ Plus App/Plus App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Plus App
+//
+//  Created by Faten Abdullh salem on 17/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
